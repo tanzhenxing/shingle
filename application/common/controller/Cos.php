631c727e49717233abc31db055717199f5e2e15d
星哥 <qc_578a83d831f3421a9e00cdc8ba7c7f7c@git.code.tencent.com>
2019-06-03 02:08:56 +0800

    更新腾讯云存储上传模块

--- application/common/controller/Cos.php
+++ application/common/controller/Cos.php
@@ -19,18 +19,16 @@ class Cos extends Controller
      * @param $server_file
      * @return array
      */
-    public function upload($local_file,$server_file)
+    public static function upload($local_file,$server_file)
     {
         // 获取配置信息
         $config = static::config();
         if($config['code']){
             return $config;
         }
-
         // 上传文件到腾讯云存储
         $cosClient = new Client($config['config']);
         $upload = $cosClient->Upload($config['bucket'], $server_file, $body = fopen($local_file, 'rb'));
-
         if(!isset($upload['Location'])){
             $result = array('code'=>1, 'message'=>'上传文件到cos 失败');
             return $result;
@@ -63,7 +61,9 @@ class Cos extends Controller
         }
         // 返回配置信息
         $bucket = $cos['bucket_name'].'-'.$cos['app_id']; //bucket的命名规则为{name}-{appid} ，此处填写的存储桶名称必须为此格式
-        $cos_config = array('region' =>$cos_region['short_name'],
+        $cos_config = array(
+            'region' =>$cos_region['name'],
+            'schema' => 'https', //协议头部，默认为http
             'credentials'=> array(
                 'appId' => $cos['app_id'],
                 'secretId'    => $cos['secret_id'],
